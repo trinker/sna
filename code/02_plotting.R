402ce42b199060fbc536f99a16be3d67d54cac78
tyler rinker <tyler.rinker@gmail.com>
2014-03-13 00:35:44 -0400

    added some stuff

--- code/02_plotting.R
+++ code/02_plotting.R
@@ -32,12 +32,26 @@ dev.off()
 
 library(gplots)
 
-pdf("plots/heatmap.pdf", width=12, height=5)
-    par(mar=c(0,0,1,0))
-    par(mfrow=c(1,2))
-    heatmap(dats[[3]]); mtext("Time 1")
-    heatmap(dats[[4]]); mtext("Time 2")
+par(mar=c(0,0,0,0))
+
+merge_pdf(2, file = "plots/heatmap.pdf", widths = c(5,5 ), heights = c(5, 5))
+heatmap(dats[[3]], main = "Time 1")
+heatmap(dats[[4]], main = "Time 2")
+
+
+
+library(GGally);library(network); library(gridExtra)
+x <- ggnet(network(dats[[3]]), label = TRUE, alpha = 1, 
+	color = "white", segment.color = "grey10") + ggtitle("Time 1")+ 
+	theme(panel.border=element_rect(colour="grey80", fill=NA))
+y <- ggnet(network(dats[[4]]), label = TRUE, alpha = 1, 
+	color = "white", segment.color = "grey10") + ggtitle("Time 2") + 
+	theme(panel.border=element_rect(colour="grey80", fill=NA))
+
+pdf("plots/ggplot_networks.pdf", width=9, height=5)
+grid.arrange(x, y, ncol=2)
 dev.off()
+
 
 
 
